7f4192dfd69f64ae192f5abf4cd30f65dcfe3073
Igor <igor.dergulev@yandex.ru>
2024-06-20 22:46:11 +0800

    Функция для генерации случайных чисел

---
+++ project_dergulev/project_dergulev.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <random>
+#include <ctime>
+#include <set>
+
+using namespace std;
+
+// Функция для генерации случайных чисел
+vector<int> generateRandomNumbers(int n, int min, int max) {
+    vector<int> numbers(n);
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dis(min, max);
+    for (int i = 0; i < n; ++i) {
+        numbers[i] = dis(gen);
+    }
+    return numbers;
+}
+
+
+int main()
+{
+    
+}
